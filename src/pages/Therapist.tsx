53fb33471d05ed2fcd2410d37982f28133635111
robynred555 <rbursnell@gmail.com>
2022-05-29 13:20:54 +0100

    Added buttons to choose appointment medium

--- src/pages/Therapist.tsx
+++ src/pages/Therapist.tsx
@@ -1,10 +1,10 @@
-import { Circle } from '@mui/icons-material';
-import { Box, Grid, Typography } from '@mui/material';
+import { Circle, Phone, VideoCall } from '@mui/icons-material';
+import { Box, Grid, IconButton, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getAvailabilitiesById } from '../actions/actions';
 import { ButtonComponent } from '../components/Button';
-import { TEAL } from '../constants/colours';
+import { DARK_GREY, GREY, TEAL, WHITE } from '../constants/colours';
 import { mockBio } from '../constants/mockInfo';
 import { AvailabilityData, TherapistInfo, AppointmentType, AppointmentMedium } from '../types';
 import { determineSelectedType } from '../utils/filterOptions';
@@ -18,6 +18,7 @@ export const TherapistPage = () => {
   const { therapist } = state as State;
   const [availabilities, setAvailabilities] = useState<AvailabilityData[] | null>(null);
   const [type, setType] = useState<AppointmentType>('one_off');
+  const [medium, setMedium] = useState<AppointmentMedium>('phone');
 
   useEffect(() => {
     const asyncFetch = async () => {
@@ -27,6 +28,12 @@ export const TherapistPage = () => {
 
     asyncFetch();
   }, [therapist]);
+
+  const determineColour = (button: AppointmentMedium, type: string) => {
+    if (type === 'text') {
+      return medium === button ? 'black' : WHITE;
+    } else return medium === button ? TEAL : DARK_GREY;
+  };
 
   return (
     <Box sx={{ flexGrow: 1, width: 1, flexDirection: 'column', justifyContent: 'center' }}>
@@ -93,6 +100,82 @@ export const TherapistPage = () => {
                     disabled={false}
                     width={'40%'}
                   />
+                </Grid>
+              </Grid>
+
+              {/* Appointment Method */}
+              <Grid container sx={{ paddingTop: '3em', flexDirection: 'column' }}>
+                <Grid item sm={12} md={12} lg={12} xl={12}>
+                  <Typography
+                    variant='h5'
+                    fontFamily={'lato, sans-serif'}
+                    sx={{ justifyContent: 'flex-start' }}
+                  >
+                    Appointment Medium
+                  </Typography>
+
+                  <Grid
+                    item
+                    sm={12}
+                    md={12}
+                    lg={12}
+                    xl={12}
+                    sx={{ justifyContent: 'space-evenly', display: 'flex' }}
+                  >
+                    <IconButton
+                      style={{
+                        minHeight: '80px',
+                        width: '40%',
+                        backgroundColor: determineColour('phone', 'button'),
+                        borderRadius: 10,
+                        flexDirection: 'column',
+                      }}
+                      onClick={() => setMedium('phone')}
+                    >
+                      <Phone
+                        style={{
+                          color: determineColour('phone', 'text'),
+                          minHeight: '90%',
+                          width: '100%',
+                          minWidth: '75px',
+                          padding: 0,
+                        }}
+                      />
+                      <Typography
+                        color={determineColour('phone', 'text')}
+                        fontFamily={'lato, sans-serif'}
+                      >
+                        Phone
+                      </Typography>
+                    </IconButton>
+
+                    <IconButton
+                      style={{
+                        minHeight: '80px',
+                        width: '40%',
+                        backgroundColor: determineColour('video', 'button'),
+                        borderRadius: 10,
+                        flexDirection: 'column',
+                      }}
+                      onClick={() => setMedium('video')}
+                    >
+                      <VideoCall
+                        style={{
+                          color: determineColour('video', 'text'),
+                          minHeight: '90%',
+                          width: '100%',
+                          minWidth: '75px',
+                          padding: 0,
+                        }}
+                      />
+                      <Typography
+                        color={determineColour('video', 'text')}
+                        fontFamily={'lato, sans-serif'}
+                      >
+                        Video
+                      </Typography>
+                    </IconButton>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
