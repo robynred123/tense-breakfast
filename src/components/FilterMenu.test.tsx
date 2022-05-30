4b82ee9b7c0ccd0f4bc9999c6604828d7d2cff88
robynred555 <rbursnell@gmail.com>
2022-05-30 13:22:35 +0100

    Updated Filter Menu Tests

--- src/components/FilterMenu.test.tsx
+++ src/components/FilterMenu.test.tsx
@@ -2,36 +2,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import { mockStore, initialState, filterOptions } from '../../mocks/mockStore';
 import { FilterMenu } from './FilterMenu';
-import { FilterOptions, ReducerState } from '../types';
-import { Store, AnyAction } from 'redux';
 
 describe('<FilterMenu />', () => {
-  const filterOptions: FilterOptions = {
-    appointmentType: [],
-    dateRange: {
-      start: null,
-      end: null,
-    },
-    specialisms: [],
-  };
   const therapistSpecialisms = ['ADHD'];
-  const initialState: ReducerState = {
-    therapists: [],
-    filteredTherapists: [],
-    filterOptions,
-    mobileFilter: false,
-  };
-  const mockStore = configureStore();
-  let store: Store<any, AnyAction>;
-  store = mockStore(initialState);
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('Should render the FilterMenu component successfully', () => {
+    const store = mockStore(initialState);
     const view = renderer.create(
       <Provider store={store}>
         <FilterMenu filterOptions={filterOptions} therapistSpecialisms={therapistSpecialisms} />
@@ -40,9 +22,17 @@ describe('<FilterMenu />', () => {
     expect(view.toJSON()).toMatchSnapshot();
     expect(view.root.findByType('h4').props.children).toEqual('Filter Results');
     expect(view.root.findAllByType('h5')[0].props.children).toEqual('Dates');
+    expect(view.root.findAllByType('label')[0].props.children[0]).toEqual('Start Date');
+    expect(view.root.findAllByType('label')[1].props.children[0]).toEqual('End Date');
+
     expect(view.root.findAllByType('h5')[1].props.children).toEqual('Appointment Type');
     const buttons = view.root.findAllByType('button');
     expect(buttons[0].props.children).toEqual([[undefined, 'One Off', undefined], null]);
     expect(buttons[1].props.children).toEqual([[undefined, 'Consultation', undefined], null]);
+
+    expect(view.root.findAllByType('h5')[2].props.children).toEqual('Specialism');
+    expect(view.root.findAllByType('input')[2].props.type).toEqual('checkbox');
+    expect(view.root.findAllByType('input')[2].props.checked).toEqual(false);
+    expect(view.root.findAllByType('label')[2].props.children[1].props.children).toEqual('ADHD');
   });
 });
